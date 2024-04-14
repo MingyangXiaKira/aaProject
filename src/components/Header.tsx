a8ceb482ae7560722e1032e23d9df85e8ac453b9
Mingyang <141198127+MingyangXiaKira@users.noreply.github.com>
2024-04-14 14:53:35 +0100

    style: widget style improvement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import Avatars from "./Avatars.tsx";
-import '../i18n.js';
-import { useTranslation } from 'react-i18next';
+import "../i18n.js";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
-
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <nav className=" flex pt-3 justify-between items-center ">
       <div className="p-5 w-20 h-20"></div>
       <div>
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            {t('header.SchoolRecommodationApp')}
+          {t("College Recommodation App")}
         </h1>
       </div>
       <div className="p-5 w-20 h-20"></div>
